3d2ea2c7f1a5684d4929354f688de5b98694359a
quanhuynhces <quan.huynh@codeenginestudio.com>
2021-05-28 01:16:04 +0700

    [Regis3-3] Implement the validation for zip code input.

--- modules/amf-search-web/src/main/java/com/liferay/search/web/portlet/action/GetZipCodeMVCActionCommand.java
+++ modules/amf-search-web/src/main/java/com/liferay/search/web/portlet/action/GetZipCodeMVCActionCommand.java
@@ -1,10 +1,15 @@
 package com.liferay.search.web.portlet.action;
 
+import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.kernel.servlet.SessionErrors;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.search.web.constants.AMFSearchPortletKeys;
 import com.liferay.search.web.constants.MVCCommandNames;
+import com.liferay.search.web.portlet.exception.ZipCodeValidationException;
+import com.liferay.search.web.portlet.validator.ZipCodeValidator;
+import com.liferay.search.web.portlet.validator.impl.ZipCodeValidatorImpl;
 import org.osgi.service.component.annotations.Component;
 
 import javax.portlet.ActionRequest;
@@ -25,9 +30,26 @@ public class GetZipCodeMVCActionCommand extends BaseMVCActionCommand {
 
         String zipCode = ParamUtil.getString(actionRequest,"zip_code");
 
+        try{
+            _zipZipCodeValidator.validate(zipCode);
             PortletSession portletSession = actionRequest.getPortletSession();
             portletSession.setAttribute("LIFERAY_SHARED_zipCode",zipCode, PortletSession.APPLICATION_SCOPE);
 
-            actionResponse.setRenderParameter("mvcRenderCommandName", MVCCommandNames.GET_ZIPCODE);
+            actionResponse.setRenderParameter("mvcRenderCommandName",MVCCommandNames.GET_ZIPCODE);
+        }catch(PortalException portalException){
+            if(portalException instanceof ZipCodeValidationException){
+
+                SessionErrors.add(actionRequest,portalException.getClass(),portalException);
+
+                PortletSession portletSession = actionRequest.getPortletSession();
+                portletSession.setAttribute("LIFERAY_SHARED_zipCode",null, PortletSession.APPLICATION_SCOPE);
+
+                actionResponse.setRenderParameter("mvcRenderCommandName",MVCCommandNames.GET_ZIPCODE);
+            } else {
+                throw portalException;
+            }
+        }
     }
+
+    ZipCodeValidator _zipZipCodeValidator = new ZipCodeValidatorImpl();
 }
--- modules/amf-search-web/src/main/resources/META-INF/resources/amfsearch/view.jsp
+++ modules/amf-search-web/src/main/resources/META-INF/resources/amfsearch/view.jsp
@@ -1,4 +1,8 @@
+<%@ page import="com.liferay.search.web.portlet.exception.ZipCodeValidationException" %>
 <%@ include file="../init.jsp" %>
+
+<liferay-ui:error exception="<%=ZipCodeValidationException.ZipCodeMustBeCorrect.class%>" message="exception.zipcode-must-be-correct"/>
+<liferay-ui:error exception="<%=ZipCodeValidationException.ZipCodeMustNotBeNull.class%>" message="exception.zipcode-must-not-be-null"/>
 
 <h1 align="center">ENTER US ZIP</h1>
 
