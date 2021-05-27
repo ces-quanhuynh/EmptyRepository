a6f1b390e3881a66b8eaed670c1f751cd3e85f36
quanhuynhces <quan.huynh@codeenginestudio.com>
2021-05-27 17:29:39 +0700

    [Regis1-4] Make the ActionCommand and RenderCommand of web module

--- modules/amf-registration/amf-registration-web/src/main/java/com/liferay/registration/web/portlet/SignUpPortlet.java
+++ modules/amf-registration/amf-registration-web/src/main/java/com/liferay/registration/web/portlet/SignUpPortlet.java
@@ -14,12 +14,12 @@ import org.osgi.service.component.annotations.Component;
 @Component(
 	immediate = true,
 	property = {
-		"com.liferay.portlet.display-category=category.sample",
+		"com.liferay.portlet.display-category=category.registration",
 		"com.liferay.portlet.header-portlet-css=/css/main.css",
 		"com.liferay.portlet.instanceable=false",
 		"javax.portlet.display-name=SignUp",
 		"javax.portlet.init-param.template-path=/",
-		"javax.portlet.init-param.view-template=/action/add_entry.jsp",
+		"javax.portlet.init-param.view-template=/signup/add_entry.jsp",
 		"javax.portlet.name=" + SignUpPortletKeys.SIGNUP,
 		"javax.portlet.resource-bundle=content.Language",
 		"javax.portlet.security-role-ref=power-user,user"
