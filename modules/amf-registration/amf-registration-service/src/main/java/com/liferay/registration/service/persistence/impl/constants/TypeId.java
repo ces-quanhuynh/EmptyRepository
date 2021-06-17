9ff0f1ce593cc1af29308354ed7a87457c8ff3e6
quanhuynhces <quan.huynh@codeenginestudio.com>
2021-06-17 09:20:07 +0700

    [Regis1-7] Fix review1, usingPhoneLocalService

---
+++ modules/amf-registration/amf-registration-service/src/main/java/com/liferay/registration/service/persistence/impl/constants/TypeId.java
@@ -0,0 +1,6 @@
+package com.liferay.registration.service.persistence.impl.constants;
+
+public class TypeId {
+    public static final long MOBILE_PHONE_ID = 11008;
+    public static final long PERSONAL_PHONE_ID = 11011;
+}
